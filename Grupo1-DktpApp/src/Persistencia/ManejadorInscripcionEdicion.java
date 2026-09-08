081bcd8c973a4ac5ba602daf542120357cf9fe99
NikShiskobcki <nicolleshiskobcki@gmail.com>
2026-09-08 20:17:56 -0300

    Update ManejadorInscripcionEdicion.java

--- Grupo1-DktpApp/src/Persistencia/ManejadorInscripcionEdicion.java
+++ Grupo1-DktpApp/src/Persistencia/ManejadorInscripcionEdicion.java
@@ -1,6 +1,8 @@
 package Persistencia;
 
 import Logica.Entidades.InscripcionEdicion;
+import Logica.excepciones.EntidadNoEncontradaException;
+import Logica.excepciones.PersistenciaException;
 import java.time.LocalDate;
 import java.util.List;
 import javax.persistence.EntityManager;
@@ -15,79 +17,76 @@ public class ManejadorInscripcionEdicion {
         this.emf = emf;
     }
     
-    public InscripcionEdicion buscarInscripcion(
-        String nicknameEstudiante,
-        String nombreEdicion) {
+    public InscripcionEdicion buscarInscripcion(String nicknameEstudiante,String nombreEdicion) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            List<InscripcionEdicion> inscripciones = em.createQuery(
+                    "SELECT i FROM InscripcionEdicion i "
+                    + "WHERE i.estudiante.nickname = :nickname "
+                    + "AND i.edicion.nombre = :nombreEdicion",
+                    InscripcionEdicion.class)
+                    .setParameter("nickname", nicknameEstudiante)
+                    .setParameter("nombreEdicion", nombreEdicion)
+                    .getResultList();
 
-    EntityManager em = emf.createEntityManager();
+            if (inscripciones.isEmpty()) {
+                return null;
+            }
 
-    try {
-        List<InscripcionEdicion> inscripciones = em.createQuery(
-                "SELECT i FROM InscripcionEdicion i "
-                + "WHERE i.estudiante.nickname = :nickname "
-                + "AND i.edicion.nombre = :nombreEdicion",
-                InscripcionEdicion.class)
-                .setParameter("nickname", nicknameEstudiante)
-                .setParameter("nombreEdicion", nombreEdicion)
-                .getResultList();
+            return inscripciones.get(0);
 
-        if (inscripciones.isEmpty()) {
-            return null;
+        } finally {
+            em.close();
         }
-
-        return inscripciones.get(0);
-
-    } finally {
-        em.close();
     }
-}
     
     public void addInscripcion(InscripcionEdicion inscripcion) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction t = em.getTransaction();
 
-    EntityManager em = emf.createEntityManager();
-    EntityTransaction t = em.getTransaction();
+        try {
+            t.begin();
+            em.persist(inscripcion);
+            t.commit();
 
-    try {
-        t.begin();
-        em.persist(inscripcion);
-        t.commit();
+        } catch (Exception e) {
+            if (t.isActive()) {
+                t.rollback();
+            }
+            throw new PersistenciaException("No se pudo guardar la inscripcion",e);
 
-    } catch (Exception e) {
-        if (t.isActive()) {
-            t.rollback();
+        } finally {
+            em.close();
         }
-        throw e;
-
-    } finally {
-        em.close();
     }
-}
     
-    public void modificarInscripcion(
-        Long idInscripcion,
-        LocalDate nuevaFecha) {
+    public void modificarInscripcion(Long idInscripcion,LocalDate nuevaFecha) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction t = em.getTransaction();
 
-    EntityManager em = emf.createEntityManager();
-    EntityTransaction t = em.getTransaction();
+        try {
+            t.begin();
 
-    try {
-        t.begin();
+            InscripcionEdicion inscripcion = em.find(InscripcionEdicion.class, idInscripcion);
+            
+            if (inscripcion == null){
+                throw new EntidadNoEncontradaException("No existe una inscripcion con esa id",null);
+            }
+            inscripcion.setFechaInscripcion(nuevaFecha);
 
-        InscripcionEdicion inscripcion =
-                em.find(InscripcionEdicion.class, idInscripcion);
+            t.commit();
 
-        inscripcion.setFechaInscripcion(nuevaFecha);
+        } catch (Exception e) {
+            if (t.isActive()) {
+                t.rollback();
+            }
+            if (e instanceof PersistenciaException){
+                throw (PersistenciaException) e;
+            }
+            throw new PersistenciaException("No se pudo modificar la inscripcion",e);
 
-        t.commit();
-
-    } catch (Exception e) {
-        if (t.isActive()) {
-            t.rollback();
+        } finally {
+            em.close();
         }
-        throw e;
-
-    } finally {
-        em.close();
     }
-}
 }
