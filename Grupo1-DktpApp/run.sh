d3c05325544f6adf5e727fd9ef16bae68c7f5c30
NikShiskobcki <87676940+NikShiskobcki@users.noreply.github.com>
2026-09-11 21:21:08 -0300

    Add build step to run.sh for cleaning and creating jar

--- Grupo1-DktpApp/run.sh
+++ Grupo1-DktpApp/run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+ant clean jar || exit 1
 java -cp "dist/Grupo1-DktpApp.jar:lib/AbsoluteLayout.jar:lib/eclipselink.jar:lib/jakarta.persistence-api-2.2.3.jar:lib/mariadb-java-client-3.5.10.jar" Presentacion.JFMain
